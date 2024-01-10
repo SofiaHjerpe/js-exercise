246e0b35916a63c300dc8685bc396d0c652e74af
Sofia Hjerpe <hjerpesofia@gmail.com>
2024-01-10 14:57:40 +0100

    add assignment 9 and 2

--- arrays/arrayAndLoop.js
+++ arrays/arrayAndLoop.js
@@ -1,14 +1,12 @@
 // 1
-let fruits = ["apples", "pineapple", "kiwi"];
-fruits.push("bananas", "pears", "plums");
+let fruits = [];
+fruits.push("apples", "pineapple", "kiwi", "bananas", "pears", "plums");
 console.log(fruits);
 // 2
 let randomNumbers = [1, 5, 35, 20, 22];
-// for (let rn in randomNumbers) {
-//   rn *= 3;
-// }
-
-//kolla sedan
+for (var i = 0; i < randomNumbers.length; i++) {
+  randomNumbers[i] = randomNumbers[i] * 3;
+}
 console.log(randomNumbers);
 // 3
 let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
@@ -24,7 +22,6 @@ console.log(higherNumbers);
 console.log([1, 5, 3, 4].reduce((a, b) => a + b, 0));
 //  5
 let cars = ["volvo", "toyota", "saab"];
-
 for (let car of cars) {
   if (car == "toyota") {
     console.log(cars[1]);
@@ -38,18 +35,18 @@ console.log(flowers);
 let bigNumbers = [1000, 1200, 1210, 1220, 1230, 1240, 1250, 1260, 1300, 1350];
 console.log(Math.max(...bigNumbers));
 // 8
-// let food = ["hamburgers", "pasta", "pizza"];
-// let morefood = food.slice();
-// console.log(morefood);
-
 let food = ["hamburgers", "pasta", "pizza"];
 let secondf = [];
 
 for (f of food) {
   if (secondf.length < 3) {
-    secondf.push(food[2]);
-    secondf.push(food[1]);
-    secondf.push(food[0]);
+    secondf.push(f);
   }
 }
 console.log(secondf);
+// 9
+let fruit = ["banana", "apple", "plum", "pear", "pineapple"];
+while (fruit.length > 0) {
+  fruit.pop();
+}
+console.log(fruit);
