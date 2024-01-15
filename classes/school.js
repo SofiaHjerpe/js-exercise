4c4bf13fb5a517d53d2ebdacefa3bfbee1892a03
Sofia Hjerpe <hjerpesofia@gmail.com>
2024-01-15 14:27:10 +0100

    add modules and way to add students to a school

--- classes/school.js
+++ classes/school.js
@@ -1,11 +1,28 @@
+import Student from "./students.js";
+let melvin = new Student("melvin", 12, "male", ["music", "swedish"]);
+let maja = new Student("maja", 14, "woman", ["math", "science"]);
+let emilia = new Student("emilia", 15, "woman", ["art", "science"]);
+let emmanuel = new Student("emmanuel", 14, "male", ["math", "science"]);
+let hedvig = new Student("hedvig", 12, "woman", ["music", "spanish"]);
+let students = {
+  melvin: melvin,
+  maja: maja,
+  emilia: emilia,
+  emmanuel: emmanuel,
+  hedvig: hedvig,
+};
 class School {
-  constructor(name, address, zipcode, city) {
+  constructor(name, address, zipcode, city, student) {
     this.name = name;
     this.address = address;
     this.zipcode = zipcode;
     this.city = city;
-    this.students = [];
+    this.student = student;
     this.teachers = [];
+  }
+  addStudent(studentName) {
+    let addStudent = students[studentName];
+    this.student.push(addStudent);
   }
 }
 
@@ -13,5 +30,10 @@ let mainSchool = new School(
   "Adolf Fredriks Musikklasser",
   "Västmannagatan 19",
   "111 56",
-  "Stockholm"
+  "Stockholm",
+  []
 );
+
+mainSchool.addStudent("maja");
+mainSchool.addStudent("melvin");
+console.log(mainSchool);
--- classes/students.js
+++ classes/students.js
@@ -1,4 +1,4 @@
-class Student {
+export default class Student {
   constructor(name, age, gender) {
     (this.name = name),
       (this.age = age),
@@ -6,8 +6,6 @@ class Student {
       (this.subjects = []);
   }
 }
-let Melvin = new Student("Melvin", 12, "male", ["music", "swedish"]);
-let Maja = new Student("Maja", 14, "woman", ["math", "science"]);
-let Emilia = new Student("Emilia", 15, "woman", ["art", "science"]);
-let Emmanuel = new Student("Emmanuel", 14, "male", ["math", "science"]);
-let Hedvig = new Student("Hedvig", 12, "woman", ["music", "spanish"]);
+
+
+
