bc6bef2939786df6062716a8ee0a755afb430bab
Sofia Hjerpe <hjerpesofia@gmail.com>
2024-01-16 10:20:40 +0100

    change all let to const for arrays

--- arrays/arrayAndLoop.js
+++ arrays/arrayAndLoop.js
@@ -1,15 +1,15 @@
 // 1
-let fruits = [];
+const fruits = [];
 fruits.push("apples", "pineapple", "kiwi", "bananas", "pears", "plums");
 console.log(fruits);
 // 2
-let randomNumbers = [1, 5, 35, 20, 22];
+const randomNumbers = [1, 5, 35, 20, 22];
 for (var i = 0; i < randomNumbers.length; i++) {
   randomNumbers[i] = randomNumbers[i] * 3;
 }
 console.log(randomNumbers);
 // 3
-let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
 let higherNumbers = [];
 
 for (let n of numbers) {
@@ -21,22 +21,22 @@ console.log(higherNumbers);
 // 4
 console.log([1, 5, 3, 4].reduce((a, b) => a + b, 0));
 //  5
-let cars = ["volvo", "toyota", "saab"];
+const cars = ["volvo", "toyota", "saab"];
 for (let car of cars) {
   if (car == "toyota") {
     console.log(cars[1]);
   }
 }
 // 6
-let flowers = ["rose", "tulip", "dandelion"];
+const flowers = ["rose", "tulip", "dandelion"];
 flowers.splice(-1, 1);
 console.log(flowers);
 // 7
-let bigNumbers = [1000, 1200, 1210, 1220, 1230, 1240, 1250, 1260, 1300, 1350];
+const bigNumbers = [1000, 1200, 1210, 1220, 1230, 1240, 1250, 1260, 1300, 1350];
 console.log(Math.max(...bigNumbers));
 // 8
-let food = ["hamburgers", "pasta", "pizza"];
-let secondf = [];
+const food = ["hamburgers", "pasta", "pizza"];
+const secondf = [];
 
 for (f of food) {
   if (secondf.length < 3) {
@@ -45,7 +45,7 @@ for (f of food) {
 }
 console.log(secondf);
 // 9
-let fruit = ["banana", "apple", "plum", "pear", "pineapple"];
+const fruit = ["banana", "apple", "plum", "pear", "pineapple"];
 while (fruit.length > 0) {
   fruit.pop();
 }
