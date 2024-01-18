2d5799a670340754631ebd97c316628f906b586f
Sofia Hjerpe <hjerpesofia@gmail.com>
2024-01-18 13:58:47 +0100

    finish obj assignment 1

--- objects/objects.js
+++ objects/objects.js
@@ -42,12 +42,12 @@ const student = {
   name: "Sofia",
   subjects: {
     art: "art",
-    languages: "languages",
-    music: "music",
-  },
-  grades: {
-    A: "A",
-    B: "B",
+
+    science: "science",
+    grades: {
+      gradeArt: "A",
+      gradeScience: "B",
+    },
   },
 };
 
@@ -57,9 +57,24 @@ person1.address.number = "26A";
 person1.address.streat = "Bellmansgatan";
 console.log(person1);
 // Modify the science grade of the student from the previous exercise.
+const studentGrade = (student.subjects.grades.gradeScience = "A");
+console.log(studentGrade);
 
 // ### Array of Objects:
 // Create an array of objects representing different books with properties like title and author.
+const books = [
+  {
+    title: "Harry P",
+    author: "JK Rowling",
+  },
+  {
+    title: "Twilight",
+    author: "Stephanie Meyer",
+  },
+];
 
 // ### Loop through Array of Objects:
 // Loop through the books array from the previous exercise and print the title and author of each book.
+for (let book of books) {
+  console.log(book.title + " " + book.author);
+}
