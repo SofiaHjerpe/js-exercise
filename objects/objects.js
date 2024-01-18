93f09aef24e51e205fc52379a09157f50973dd1d
Sofia Hjerpe <hjerpesofia@gmail.com>
2024-01-18 14:00:07 +0100

    remove some comments

--- objects/objects.js
+++ objects/objects.js
@@ -1,5 +1,4 @@
 // ### Create an Object:
-
 const person = {
   name: "Sofia",
   age: 24,
@@ -7,12 +6,10 @@ const person = {
 };
 // ### Access Object Properties:
 console.log("Sofia Hjerpe: " + person.name + " " + person.age + " " + person.city);
-
 // ### Modify Object Properties:
 person.age = 25;
 person.email = "sofia.hjerpe@yahoo.com";
 console.log(person);
-
 // ### Object with Methods:
 const car = {
   make: "Volvo",
@@ -21,12 +18,10 @@ const car = {
     console.log("year = 2024, color = red");
   },
 };
-
 // ### Loop through Object Properties:
 for (let p in person) {
   console.log(person[p]);
 }
-
 // ### Object with Nested Properties:
 const person1 = {
   name: "Sofia",
@@ -37,7 +32,6 @@ const person1 = {
   },
 };
 
-// Create an object representing a student with nested properties for subjects and grades.
 const student = {
   name: "Sofia",
   subjects: {
@@ -56,12 +50,11 @@ const student = {
 person1.address.number = "26A";
 person1.address.streat = "Bellmansgatan";
 console.log(person1);
-// Modify the science grade of the student from the previous exercise.
+
 const studentGrade = (student.subjects.grades.gradeScience = "A");
 console.log(studentGrade);
 
 // ### Array of Objects:
-// Create an array of objects representing different books with properties like title and author.
 const books = [
   {
     title: "Harry P",
@@ -74,7 +67,6 @@ const books = [
 ];
 
 // ### Loop through Array of Objects:
-// Loop through the books array from the previous exercise and print the title and author of each book.
 for (let book of books) {
   console.log(book.title + " " + book.author);
 }
