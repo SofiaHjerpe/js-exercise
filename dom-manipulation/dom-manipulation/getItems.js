fc56c59eb75d468b6e9346232bcc79672870dba5
Sofia Hjerpe <hjerpesofia@gmail.com>
2024-01-24 14:30:25 +0100

    get ingredients and instructions

--- dom-manipulation/dom-manipulation/getItems.js
+++ dom-manipulation/dom-manipulation/getItems.js
@@ -16,9 +16,50 @@ let url = image.baseURI;
 let height = image.scrollHeight;
 let width = image.scrollWidth;
 const imageWidth = {
-    url: url,
-    height: height, 
-    width: width
-}
+  url: url,
+  height: height,
+  width: width,
+};
 console.log(imageWidth);
-
+// 1
+const ingredientsFirstList = document.querySelectorAll(".ingredients-list-bottom li");
+const ingredientsSecondList = document.querySelectorAll(".ingredients-list-paste li");
+const ingredients = ingredientsFirstList.length + ingredientsSecondList.length;
+console.log("Nr of ingredients: " + ingredients);
+// 2
+console.log(ingredientsSecondList[3]);
+// 3
+const instructions = document.querySelectorAll(".instructions-list li");
+const arrayOfInstuctions = [
+  {
+    order: 1,
+    text: instructions[0].innerText,
+  },
+  {
+    order: 2,
+    text: instructions[1].innerText,
+  },
+  {
+    order: 3,
+    text: instructions[2].innerText,
+  },
+  {
+    order: 4,
+    text: instructions[3].innerText,
+  },
+  {
+    order: 5,
+    text: instructions[4].innerText,
+  },
+  {
+    order: 6,
+    text: instructions[5].innerText,
+  },
+  {
+    order: 7,
+    text: instructions[6].innerText,
+  },
+];
+arrayOfInstuctions.forEach(function (instruction) {
+  console.log(instruction.text);
+});
